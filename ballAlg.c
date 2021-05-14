433a8afb8fd1758798cddba5a2a8f189624c1981
Inês Ferreira <ines.alves.ferreira@tecnico.ulisboa.pt>
2021-05-14 22:05:48 +0100

    finish sorting algorithm

--- ballAlg.c
+++ ballAlg.c
@@ -72,17 +72,14 @@ int partition(double pivot, int l, int r) {
     int j;
     for (j = l; j < r; j++)
     {
-        if (proj[j] < pivot)
+        if (proj[j] <= pivot)
         {
             i++;
             swap(i, j);
         }
     }
 
-    if (proj[i+1] == pivot)
-        return i+2;
-    else
-        return i+1;
+    return i+1;
 
 }
 
@@ -180,68 +177,90 @@ void get_median(int l, int r, int p, MPI_Comm comm){
     swap(0, min_index);
     pivots[0] = proj[0];
 
-    printf("id: %d, pivots[0]: %f\n", id, pivots[0]);
-    fflush(stdout);
-
-    // MPI_Barrier(MPI_COMM_WORLD);
-    // exit(0);
-
-    // Outros dois pivots são escolhido como o primeiro elemento dado um índice
-    int idx = -1;
-    for(int i = 1; i < p; ++i) {
-        idx = quickselect_seq((r-l)/p-1, (i-1)*(r-l)/p+1, r);
-        pivots[i] = proj[idx];
-    }
-
-    // print_point(proj, 9);
-
-    printf("id: %d, pivots[0]: %f, pivots[1]: %f, pivots[2]: %f\n", id, pivots[0], pivots[1], pivots[2]);
-    fflush(stdout);
+    // printf("id: %d, pivots[0]: %f\n", id, pivots[0]);
+    // fflush(stdout);
 
     MPI_Barrier(MPI_COMM_WORLD);
     // exit(0);
 
+    // Outros dois pivots são escolhido como o primeiro elemento dado um índice
+    int idx = -1;
+    int piv_idx, prev_piv_idx = 0;
+    for(int i = 1; i < p; ++i) {
+        piv_idx = i * (r-l) / p - prev_piv_idx - 1;
+        idx = quickselect_seq(piv_idx, prev_piv_idx+1, r);
+        prev_piv_idx = idx;
+        pivots[i] = proj[idx];
+    }
+
+    // if (!id)
+    //     print_point(proj, 6);  
+    // else
+    //     print_point(proj, 7);
+
+    // printf("id: %d, pivots[0]: %f, pivots[1]: %f, pivots[2]: %f, pivots[3]: %f\n", id, pivots[0], pivots[1], pivots[2], pivots[3]);
+    // fflush(stdout);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
     // Enviar os pivots locais para o primeiro processador
     MPI_Gather(pivots, p, MPI_DOUBLE, glob_pivots, p, MPI_DOUBLE, 0, comm);
 
-    if (!id) {
-        print_point(glob_pivots, p*p);
-    }
+    // if (!id) {
+    //     print_point(glob_pivots, p*p);
+    // }
 
     // O primeiro processador calcula p-1 pivots utilizando quick select
     if(!id){
+        prev_piv_idx = -1;
         for(int i = 1; i < p; ++i) {
-            pivots[i-1] = quickselect_seq_2((r-l)/p-1, (i-1)*(r-l)/p+1, r);
+            piv_idx = i * p - prev_piv_idx - 1;
+            pivots[i-1] = quickselect_seq_2(piv_idx, prev_piv_idx+1, p*p);
+            // printf("pivots[i-1]: %f, piv_idx: %d, prev_piv_idx: %d\n", pivots[i-1], piv_idx, prev_piv_idx);
+            prev_piv_idx += piv_idx + 1;
         }
-        // print_point(glob_pivots, 9);
+        // print_point(glob_pivots, 16);
     }
 
-    printf("id: %d, pivots[0]: %f, pivots[1]: %f\n", id, pivots[0], pivots[1]);
-    fflush(stdout);
+    MPI_Barrier(MPI_COMM_WORLD);
+    // exit(0);
+
+    // printf("id: %d, pivots[0]: %f, pivots[1]: %f\n", id, pivots[0], pivots[1]);
+    // fflush(stdout);
 
     // Enviam-se esses pivots para todos os processadores
     MPI_Bcast(pivots, p-1, MPI_DOUBLE, 0, comm);
 
-    // print_point(proj, 9);  
+    // if (!id)
+    //     print_point(proj, 6);  
+    // else
+    //     print_point(proj, 7);
 
     // Partições segundos os pivots
     send_counts[0] = partition(pivots[0], 0, r);
     int sum = send_counts[0];
     for(int i = 1; i < p-1; i++) {
-        send_counts[i] = partition(pivots[i], (int)send_counts[i-1]+1, r) - send_counts[i-1];
+        send_counts[i] = partition(pivots[i], sum, r) - sum;
         sum += send_counts[i];
     }
+
     send_counts[p-1] = r - sum;
 
-    printf("id: %d, send_counts[0]: %d, send_counts[1]: %d, send_counts[2]: %d\n", id, send_counts[0], send_counts[1], send_counts[2]);
-    fflush(stdout);
+    // printf("id: %d, send_counts[0]: %d, send_counts[1]: %d, send_counts[2]: %d, send_counts[3]: %d\n", id, send_counts[0], send_counts[1], send_counts[2], send_counts[3]);
+    // fflush(stdout);
+
+    // if (!id)
+    //     print_point(proj, 6);  
+    // else
+    //     print_point(proj, 7);
 
     // print_point(proj, 9);    
 
     // Array of sizes
     MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, comm);
 
-    printf("id: %d, recv_counts[0]: %d, recv_counts[1]: %d, recv_counts[2]: %d\n", id, recv_counts[0], recv_counts[1], recv_counts[2]);
+    // printf("id: %d, recv_counts[0]: %d, recv_counts[1]: %d, recv_counts[2]: %d, recv_counts[3]: %d\n", id, recv_counts[0], recv_counts[1], recv_counts[2], recv_counts[3]);
+    // fflush(stdout);
 
     int sum1 = 0;
     int sum2 = 0;
@@ -252,17 +271,17 @@ void get_median(int l, int r, int p, MPI_Comm comm){
         sum2 += send_counts[i];
     }
 
-    printf("id: %d, sum: %d\n", id, sum1);
-
-    // MPI_Barrier(MPI_COMM_WORLD);
-    // exit(0);
-
-    printf("id: %d, recv_displs[0]: %d, recv_displs[1]: %d, recv_displs[2]: %d\n", id, recv_displs[0], recv_displs[1], recv_displs[2]);
-    printf("id: %d, send_displs[0]: %d, send_displs[1]: %d, send_displs[2]: %d\n", id, send_displs[0], send_displs[1], send_displs[2]);
-
-    printf("aux: %d\n", aux);
+    printf("id: %d, sum1: %d\n", id, sum1);
 
     MPI_Barrier(MPI_COMM_WORLD);
+    // exit(0);
+
+    // printf("id: %d, recv_displs[0]: %d, recv_displs[1]: %d, recv_displs[2]: %d, recv_displs[3]: %d\n", id, recv_displs[0], recv_displs[1], recv_displs[2], recv_displs[3]);
+    // fflush(stdout);
+    // printf("id: %d, send_displs[0]: %d, send_displs[1]: %d, send_displs[2]: %d, send_displs[3]: %d\n", id, send_displs[0], send_displs[1], send_displs[2], send_displs[3]);
+    // fflush(stdout);
+    // printf("aux: %d\n", aux);
+    // fflush(stdout);
 
     //Fazer free disto to fim desta função
     // if (sum1 > aux) {
@@ -274,7 +293,7 @@ void get_median(int l, int r, int p, MPI_Comm comm){
     // }
 
     MPI_Alltoallv(proj, send_counts, send_displs, MPI_DOUBLE, recv_buffer, recv_counts, recv_displs, MPI_DOUBLE, comm);
-    print_point(recv_buffer, sum1); 
+    // print_point(recv_buffer, sum1); 
 
     free(proj);
     proj = recv_buffer;
@@ -286,10 +305,10 @@ void get_median(int l, int r, int p, MPI_Comm comm){
         recv_displs[i] *= n_dims;
     }
 
-    printf("id: %d, recv_displs[0]: %d, recv_displs[1]: %d, recv_displs[2]: %d\n", id, recv_displs[0], recv_displs[1], recv_displs[2]);
-    printf("id: %d, send_displs[0]: %d, send_displs[1]: %d, send_displs[2]: %d\n", id, send_displs[0], send_displs[1], send_displs[2]);
-    printf("id: %d, recv_counts[0]: %d, recv_counts[1]: %d, recv_counts[2]: %d\n", id, recv_counts[0], recv_counts[1], recv_counts[2]);
-    printf("id: %d, send_counts[0]: %d, send_counts[1]: %d, send_counts[2]: %d\n", id, send_counts[0], send_counts[1], send_counts[2]);
+    // printf("id: %d, recv_displs[0]: %d, recv_displs[1]: %d, recv_displs[2]: %d\n", id, recv_displs[0], recv_displs[1], recv_displs[2]);
+    // printf("id: %d, send_displs[0]: %d, send_displs[1]: %d, send_displs[2]: %d\n", id, send_displs[0], send_displs[1], send_displs[2]);
+    // printf("id: %d, recv_counts[0]: %d, recv_counts[1]: %d, recv_counts[2]: %d\n", id, recv_counts[0], recv_counts[1], recv_counts[2]);
+    // printf("id: %d, send_counts[0]: %d, send_counts[1]: %d, send_counts[2]: %d\n", id, send_counts[0], send_counts[1], send_counts[2]);
 
     // for (int i = 0; i < BLOCK_SIZE(i,p,np); i++)
     //     print_point(pt_arr[i], n_dims);
@@ -297,8 +316,8 @@ void get_median(int l, int r, int p, MPI_Comm comm){
     MPI_Alltoallv(p_aux, send_counts, send_displs, MPI_DOUBLE, p_aux_3, recv_counts, recv_displs, MPI_DOUBLE, comm);
     for (long i = 0; i < sum1; i++)
         pt_arr[i] = &p_aux_3[i * n_dims];
-    for (int i = 0; i < sum1; i++)
-        print_point(p_aux_3+i*n_dims, n_dims);
+    // for (int i = 0; i < sum1; i++)
+    //     print_point(p_aux_3+i*n_dims, n_dims);
 
     int size = (id < p/2 + (p%2)) ? sum1 : 0;
     int total_size = 0;
@@ -309,41 +328,191 @@ void get_median(int l, int r, int p, MPI_Comm comm){
     // MPI_Barrier(MPI_COMM_WORLD);
     // exit(0);
 
-    int m1 = -1, m2 = -1;
+    // print_point(proj, sum1);
 
-    if (id == p/2) {
-        int min_proj;
-        if (p%2 == 1) {
-            m1 = quickselect_seq((np)/2-(total_size-sum1), 0, sum1);
-            printf("id: %d, (np)/2-(total_size-sum1): %ld\n", id, (np)/2-(total_size-sum1));
-            if (np%2 == 0) {
+    int m1 = -1, m2 = -1;
+    int min_proj;
+    double u;
+    if ((id == p/2) && (p%2 == 1)) {
+        m1 = quickselect_seq(np/2-!(np%2)-(total_size-sum1), 0, sum1);
+        // printf("id: %d, (np)/2-(total_size-sum1): %ld\n", id, np/2-!(np%2)-(total_size-sum1));
+        if (np%2 == 0) {
+            min_proj = proj[m1+1];
+            m2 = m1+1;
+            for (int i = m1+1; i < r; i++)
+                if (proj[i] < min_proj) {
+                    min_proj = proj[i];
+                    m2 = i;
+                }
+            swap(m2, m1+1);
+            u = (proj[m1] + proj[m1+1])/2;
+            // Enviar de m1+1 a r para a direita
+            MPI_Send(proj+m1+1, sum1-(m1+1), MPI_DOUBLE, id+1, 0, comm);
+            MPI_Send(p_aux_3+(m1+1)*n_dims, (sum1-(m1+1))*n_dims, MPI_DOUBLE, id+1, 1, comm);
+            sum1 = m1+1;
+        }
+        else {
+            u = proj[m1];
+            // printf("id: %d, u: %f\n", id, u);
+            // fflush(stdout);
+            // Enviar de m1 a r para a direita
+            MPI_Send(proj+m1, sum1-m1, MPI_DOUBLE, id+1, 0, comm);
+            MPI_Send(p_aux_3+m1*n_dims, (sum1-m1)*n_dims, MPI_DOUBLE, id+1, 1, comm);
+            sum1 = m1;
+        }
+        // printf("id: %d, m1: %d, m2: %d, proj[m1]: %f\n", id, m1, m2, proj[m1]);
+    }
+    else if ((id == p/2+1) && (p%2 == 1)) {
+        u = 0;
+        MPI_Recv(proj+sum1, total_size-(np/2), MPI_DOUBLE, id-1, 0, comm, MPI_STATUS_IGNORE);        
+        MPI_Recv(p_aux_3+sum1*n_dims, (total_size-(np/2))*n_dims, MPI_DOUBLE, id-1, 1, comm, MPI_STATUS_IGNORE);
+        sum1 += total_size-(np/2);
+        for (long i = 0; i < sum1; i++) {
+            pt_arr[i] = &p_aux_3[i * n_dims];
+        }
+    }
+    else if ((np%2 == 1) && (p%2 == 0)) {
+        if (id == p/2 - 1) {
+            if (total_size >= np/2) {
+                m1 = quickselect_seq((np)/2-(total_size-sum1), 0, sum1);
+                u = proj[m1];
+                MPI_Send(proj+m1, sum1-m1, MPI_DOUBLE, id+1, 0, comm);
+                MPI_Send(p_aux_3+m1*n_dims, (sum1-m1)*n_dims, MPI_DOUBLE, id+1, 1, comm);
+                sum1 = m1;
+            }
+            else {
+                u = 0;
+                fflush(stdout);
+                MPI_Recv(proj+sum1, (np/2)-total_size, MPI_DOUBLE, id+1, 0, comm, MPI_STATUS_IGNORE);        
+                MPI_Recv(p_aux_3+sum1*n_dims, ((np/2)-total_size)*n_dims, MPI_DOUBLE, id-1, 1, comm, MPI_STATUS_IGNORE);
+                sum1 += (np/2)-total_size;
+                for (long i = 0; i < sum1; i++) {
+                    pt_arr[i] = &p_aux_3[i * n_dims];
+                }
+            }
+        }
+        else if (id == p/2) {
+            if (total_size < np/2) {
+                m1 = quickselect_seq((np)/2-(total_size), 0, sum1);
+                u = proj[m1];
+                MPI_Send(proj, m1, MPI_DOUBLE, id-1, 0, comm);
+                MPI_Send(p_aux_3, m1*n_dims, MPI_DOUBLE, id-1, 1, comm);
+                sum1 = sum1-m1;
+                for (int i = 0; i < sum1; i++) {
+                    proj[i] = proj[i+m1+1];
+                    // pt_arr[i] = &p_aux_3[(i+m1+1) * n_dims];
+                    pt_arr[i] = pt_arr[i+m1+1];
+                }
+            }
+            else {
+                u = 0;
+                MPI_Recv(proj+sum1, total_size-(np/2), MPI_DOUBLE, id+1, 0, comm, MPI_STATUS_IGNORE);        
+                MPI_Recv(p_aux_3+sum1*n_dims, (total_size-(np/2))*n_dims, MPI_DOUBLE, id+1, 1, comm, MPI_STATUS_IGNORE);
+                sum1 += total_size-(np/2);
+                for (long i = 0; i < sum1; i++) {
+                    pt_arr[i] = &p_aux_3[i * n_dims];
+                }
+            }
+        }
+        else {
+            u = 0;
+        }
+    }
+    else if ((np%2 == 0) && (p%2 == 0)) {
+        if (id == p/2 - 1) {
+            if (total_size >= np/2) {
+                m1 = quickselect_seq((np)/2-1-(total_size-sum1), 0, sum1);
+                if (total_size != np/2) {
+                    min_proj = proj[m1+1];
+                    m2 = m1+1;
+                    for (int i = m1+1; i < r; i++)
+                        if (proj[i] < min_proj) {
+                            min_proj = proj[i];
+                            m2 = i;
+                        }
+                    swap(m2, m1+1);
+                    u = (proj[m1]+proj[m1+1])/2;
+                    MPI_Send(proj+m2, sum1-m2, MPI_DOUBLE, id+1, 0, comm);
+                    MPI_Send(p_aux_3+m2*n_dims, (sum1-m2)*n_dims, MPI_DOUBLE, id+1, 1, comm);
+                    sum1 = sum1-m2;
+                }
+                else {
+                    u = proj[m1]/2;
+                }
+            }
+            else {
+                u = 0;
+                MPI_Recv(proj+sum1, (np/2)-total_size, MPI_DOUBLE, id+1, 0, comm, MPI_STATUS_IGNORE);        
+                MPI_Recv(p_aux_3+sum1*n_dims, ((np/2)-total_size)*n_dims, MPI_DOUBLE, id+1, 1, comm, MPI_STATUS_IGNORE);
+                sum1 += (np/2)-total_size;
+                for (long i = 0; i < sum1; i++) {
+                    pt_arr[i] = &p_aux_3[i * n_dims];
+                }
+            }
+        }
+        else if (id == p/2) {
+            if (total_size < np/2) {
+                m1 = quickselect_seq(np/2-1-total_size, 0, sum1);
                 min_proj = proj[m1+1];
+                m2 = m1+1;
                 for (int i = m1+1; i < r; i++)
                     if (proj[i] < min_proj) {
                         min_proj = proj[i];
                         m2 = i;
                     }
                 swap(m2, m1+1);
+                u = (proj[m1]+proj[m1+1])/2;
+                MPI_Send(proj, m1+1, MPI_DOUBLE, id-1, 0, comm);
+                MPI_Send(p_aux_3, (m1+1)*n_dims, MPI_DOUBLE, id-1, 1, comm);
+                sum1 = sum1-(m1+1);
+                for (int i = 0; i < sum1; i++) {
+                    proj[i] = proj[i+m1+1];
+                    // pt_arr[i] = &p_aux_3[(i+m1+1) * n_dims];
+                    pt_arr[i] = pt_arr[i+m1+1];
+                }
             }
-            if (m2 == -1) {
-                // Enviar de m1 a r para a direita
-                MPI_Send(proj+m1, sum1-m1, MPI_DOUBLE, id+1, 0, comm);
-                sum1 = m1;
+            else if (total_size == np/2) {
+                m1 = -1;
+                min_proj = proj[m1+1];
+                m2 = m1+1;
+                for (int i = m1+1; i < r; i++)
+                    if (proj[i] < min_proj) {
+                        min_proj = proj[i];
+                        m2 = i;
+                    }
+                swap(m2, m1+1);
+                u = proj[0]/2;
             }
             else {
-                // Enviar de m2 a r para a direita
-                MPI_Send(proj+m2, sum1-m2, MPI_DOUBLE, id+1, 0, comm);
-                sum1 = m2;
+                u = 0;
+                MPI_Recv(proj+sum1, total_size-(np/2), MPI_DOUBLE, id-1, 0, comm, MPI_STATUS_IGNORE);        
+                MPI_Recv(p_aux_3+sum1*n_dims, (total_size-(np/2))*n_dims, MPI_DOUBLE, id-1, 1, comm, MPI_STATUS_IGNORE);
+                sum1 += total_size-(np/2);
+                for (long i = 0; i < sum1; i++) {
+                    pt_arr[i] = &p_aux_3[i * n_dims];
+                }
             }
-        } 
-        printf("id: %d, m1: %d, m2: %d, proj[m1]: %f\n", id, m1, m2, proj[m1]);
+        }
     }
-    else if (id == p/2+1) {
-        MPI_Recv(proj+sum1, total_size-(np/2), MPI_DOUBLE, id-1, 0, comm, MPI_STATUS_IGNORE);        
-        sum1 += total_size-(np/2);
+    else {
+        u = 0;
     }
 
-    print_point(proj, sum1); 
+    print_point(proj, sum1);
+
+    for (int i = 0; i < sum1; i++)
+        print_point(pt_arr[i], n_dims);
+
+    if(id == 1) {
+        printf("id: %d, u: %f\n", id, u);
+        fflush(stdout);
+    }
+
+    double u_aux;
+    MPI_Allreduce(&u, &u_aux, 1, MPI_DOUBLE, MPI_SUM, comm);
+    u = u_aux;
+
+    printf("id: %d, m1: %d, m2: %d, u: %f\n", id, m1, m2, u);
 
     MPI_Barrier(MPI_COMM_WORLD);
     exit(0);
@@ -365,7 +534,7 @@ int main(int argc, char **argv) {
     unsigned seed = atoi(argv[3]);
     srandom(seed);
 
-    aux = ceil(np/p) * 5/4;
+    aux = ceil(np/p) * 5/2;
     p_aux = (double *)malloc(n_dims * aux * sizeof(double));
     pt_arr = (double **)malloc(aux * sizeof(double *));
     for (long i = 0; i < aux; i++)
@@ -390,31 +559,33 @@ int main(int argc, char **argv) {
         proj[3] = 6;
         proj[4] = 93;
         proj[5] = 39;
-        proj[6] = 72;
-        proj[7] = 91;
-        proj[8] = 14;
     }
     else if (id == 1) {
-        proj[0] = 36;
-        proj[1] = 69;
-        proj[2] = 40;
-        proj[3] = 89;
-        proj[4] = 61;
-        proj[5] = 97;
-        proj[6] = 12;
-        proj[7] = 21;
-        proj[8] = 54;       
+        proj[0] = 72;
+        proj[1] = 91;
+        proj[2] = 14;
+        proj[3] = 36;
+        proj[4] = 69;
+        proj[5] = 40;
+        proj[6] = 89;
     }
-    else {
-        proj[0] = 53;
+    else if (id == 2) {
+        proj[0] = 61;
         proj[1] = 97;
-        proj[2] = 84;
-        proj[3] = 58;
-        proj[4] = 32;
-        proj[5] = 27;
-        proj[6] = 33;
-        proj[7] = 72;
-        proj[8] = 20;         
+        proj[2] = 12;
+        proj[3] = 21;
+        proj[4] = 54;           
+        proj[5] = 53;
+        proj[6] = 97;
+    }
+    else if (id == 3) {
+        proj[0] = 84;
+        proj[1] = 58;
+        proj[2] = 32;
+        proj[3] = 27;
+        proj[4] = 33;
+        proj[5] = 72;
+        proj[6] = 20;
     }
 
     int BL = BLOCK_LOW(id, p, np);
